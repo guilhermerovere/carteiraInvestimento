143ec8dfae34a1d74281f14807510c4cae29301d
guilhermerovere <guilhermeroveredela@hotmail.com>
2026-08-25 21:57:50 -0300

    Pasta raiz adicionada

---
+++ carteiraInvestimento/src/main/java/com/carteira/carteiraInvestimento/CarteiraInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.carteira.carteiraInvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraInvestimentoApplication.class, args);
+	}
+
+}
---
+++ carteiraInvestimento/src/test/java/com/carteira/carteiraInvestimento/CarteiraInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteira.carteiraInvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
